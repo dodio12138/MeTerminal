fdc5637068d9c7c1e93e211dcf7d47539aeec2fe
dodio12138 <1159715241@qq.com>
2023-07-30 04:40:25 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -216,11 +216,15 @@ document.addEventListener('DOMContentLoaded', function() {
     while (terminalOutput.firstChild) {
       terminalOutput.removeChild(terminalOutput.firstChild);
     }
-    terminalOutput.textContent = '>> Shutting down......' + '\n';
-    printTextOneByOne(terminalOutput, 20 ,2);
+    terminalOutput.textContent = '>> Clearing clipboard...' + '\n'
+    + '   Clearing web cache...' + '\n'
+    + '   Resetting the time...' + '\n'
+    + '   Resynchronising the timeline in progress...' + '\n'
+    + '   Disconnecting MeTerminal...' + '\n';
+    printTextOneByOne(terminalOutput, 10 ,2);
     setTimeout(function() {
       window.close(); // 在定时器回调函数中执行关闭操作
-    }, 2000); // 1000毫秒即为1秒
+    }, 4000); // 1000毫秒即为1秒
   }
 
     setInterval(updateTime, 1000); // 每隔一秒钟调用 updateTime 函数
